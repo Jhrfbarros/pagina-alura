6e0d63451018f31eb9789aad542e89192a55921a
Joao Henrique <jhrfbarros@gmail.com>
2021-09-06 12:57:32 -0300

    Inclusao da pattern no telefone, pagina do produto unitario e nodemail

---
+++ js/mail.js
@@ -0,0 +1,23 @@
+const nodemailer = require("nodemailer");
+
+let transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com"
+  port: 587
+  secure: true
+  auth:{
+    user: "kodemenbarros@gmail.com"
+    pass: "k0d&123456"
+  }
+});
+
+transporter.sendMail({
+  from: "kodemenbarros@gmail.com"
+  to: "jhrfbarros@gmail.com"
+  subject: "subject"
+  text:"text"
+  html: <a href = "contato.html" </a>
+}).then(message => {
+  console.log(message);
+}).cath(err => {
+  console.log(err);
+})
